58a5969e9ae3bb848817c3665ebca96a5772ab75
Nguyen Dinh Hung <Hung.Nguyen@Nguyens-MacBook-Pro-2.local>
2017-04-18 22:11:31 +0700

    crawl

---
+++ maCrawler.js
@@ -0,0 +1,61 @@
+var schedule = require('node-schedule');
+var axios = require('axios');
+var dateFormat = require('dateformat');
+var MongoClient = require('mongodb').MongoClient;
+var PubSub = require('./PubSub.js');
+
+var url = 'mongodb://103.63.109.80:27017/notiii';
+
+MongoClient.connect(url, function(err, db) {
+  	console.log("Connected correctly to server");
+  	var collection = db.collection('mastock');
+ 	PubSub.subscribe('ma_data', function(stocks){
+ 		stocks.forEach(function(stock, i){
+ 			stock['_id'] = stock.id;
+ 			collection.index(stock);
+ 		});
+ 	});
+});
+
+
+
+var getStockData = function(stocks, index){
+
+	if(stocks.length == index) {
+		console.log("Finish crawl");
+		return;
+	}
+
+	var symbol = stocks[index].symbol;
+	index++;
+
+	var to = new Date();
+	var from = new Date();
+ 	from.setDate(from.getDate()-50);
+
+ 	var fromDate = dateFormat(from, "yyyy-mm-dd");
+ 	var toDate = dateFormat(to, "yyyy-mm-dd");
+
+	var url = 'https://finfoapi-hn.vndirect.com.vn/stocks/adPrice?symbols=' + symbol + '&fromDate=' + fromDate + '&toDate=' + toDate;
+	
+	console.log('get data from ' + url);
+	axios.get(url).then(function(response){
+		console.log(response.data);
+		PubSub.publish("ma_data",response.data);
+		getStockData(stocks, index);
+	});
+}
+
+var crawl = function() {
+	console.log('Start crawl from finfo')
+	axios.get('https://finfoapi-hn.vndirect.com.vn/stocks').then(function(response){
+		var stocks = response.data.data;
+		getStockData(stocks, 0);
+	});
+}
+
+var job = schedule.scheduleJob('1 0 0 * * *', function(){
+  	crawl();
+});
+
+crawl();
