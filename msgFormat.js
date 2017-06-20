a2c2b9919cb248009401c3f4557e320d7a4b10f4
Nguyen Dinh Hung <Hung.Nguyen@Nguyens-MacBook-Pro-2.local>
2017-06-20 17:31:35 +0700

    change format

--- msgFormat.js
+++ msgFormat.js
@@ -1,28 +1,29 @@
 //var noti = {"id":"SG_3300496804574921227","status":"CANCELLED","createdAt":1494413401290,"triggeredAt":1494415152488,"expiryDate":1495472399000,"symbols":["VND"],"userID":null,"operators":[{"@class":"notiii.sig.model.operator.SingleFunction","symbol":"VND","field":"matchedPrice","operator":">=","value":"15700.0"}],"signalID":"SG_3300496804574921227"}
 
-var getConditionText = function(condition){
+var getConditionText = function(condition, isAddSymbol){
 	var text = '';
+	var preText = isAddSymbol ? condition.symbol : '';
 	if (condition.field == "matchedPrice" ) {
 		var price = parseFloat(condition.value)/1000;
-		return condition.symbol + ' giá ' + condition.operator + ' ' + price;
+		return preText + ' giá ' + condition.operator + ' ' + price;
 	}
 	if (condition.field == "accumulatedVol" ) {
-		return condition.symbol + ' khối lượng ' + condition.operator + ' ' + parseInt(condition.value);
+		return preText + ' khối lượng ' + condition.operator + ' ' + parseInt(condition.value);
 	}
 	if (condition.field == "ma20_x_ma50_cu" ) {
-		return condition.symbol + " ma20 cắt lên ma50"
+		return preText + " MA20 cắt lên MA50"
 	}
 	if (condition.field == "ma20_x_ma50_cd") {
-		return condition.symbol + " ma20 cắt xuống ma50"
+		return preText + " MA20 cắt xuống MA50"
 	}
-	return condition.symbol + ' ' + condition.field + ' ' + condition.operator + ' ' + condition.value;
+	return preText + ' ' + condition.field + ' ' + condition.operator + ' ' + condition.value;
 }
 
 var getNotiMessage = function(msg){
 	var text = 'Thoả mãn điều kiện: ';
 	for (var i=0; i<msg.operators.length; i++) {
 		if (i>0) text += ', '
-		text += getConditionText(msg.operators[i]);
+		text += getConditionText(msg.operators[i], i==0);
 	}
 
 	return text;
